74d04704cbffd6ed853013a6d428ada1a5652d29
José Marcos <jm.prado.dev@gmail.com>
2025-03-23 19:59:05 -0300

    while adicionado + bug fix no labirinto 15x15
    o programa agora roda até que a opção 4 (sair) seja selecionada.
    (possível bug: não lida com outros tipos de dados diferentes de int - a resolver)

--- Codigo Fonte/labirinto_v1.c
+++ Codigo Fonte/labirinto_v1.c
@@ -12,6 +12,8 @@ void salvaLabirinto (char* nomeLab, char labirinto[20][20], int linhas) {
 	}
 	
 	fclose(lab);
+	
+	printf("\nArquivo Salvo!\n");
 }
 
 int main(int argc, char** argv){ 
@@ -29,23 +31,26 @@ int main(int argc, char** argv){
 		printf("%s\n", labirinto[i]);
 	}
 
+	int sair = 0;
+	while (sair == 0){
+		printf("\n\n");
 	
-	printf("\n\n");
-	
-	printf("Selecione o modo desejado:\n");
-	printf("1. Resolver o labirinto com uma tentativa.\n");
-	printf("2. Resolver o labirinto ate obter sucesso.\n");
-	printf("3. Salvar o labirinto resolvido em um arquivo.\n");
-	printf("4. Sair do programa.\n");
-	
-	int modo;
-	scanf("%i", &modo);
+		printf("Selecione o modo desejado:\n");
+		printf("1. Resolver o labirinto com uma tentativa.\n");
+		printf("2. Resolver o labirinto ate obter sucesso.\n");
+		printf("3. Salvar o labirinto resolvido em um arquivo.\n");
+		printf("4. Sair do programa.\n");
+		
+		int modo;
+		scanf("%i", &modo);
 
-	switch (modo)
-	{
-		case 3: salvaLabirinto(argv[1], labirinto, n); break;
-		case 4: return 0; break;
+		switch (modo)
+		{
+			case 3: salvaLabirinto(argv[1], labirinto, n); break;
+			case 4: sair = 1; break;
+			default: printf ("\nValor invalido!\n");
+		}
 	}
 	
 	return 0;
-	}
+}
