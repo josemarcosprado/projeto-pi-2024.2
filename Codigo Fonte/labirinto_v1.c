b89841641593e73a1c8deff617d0a7a680cc1dce
José Marcos <jm.prado.dev@gmail.com>
2025-03-23 19:21:15 -0300

    Entrega Semana 1 concluída
    recebe o nome do arquivo na linha de comando
    salva o labirinto com esse nome

--- Codigo Fonte/labirinto_v1.c
+++ Codigo Fonte/labirinto_v1.c
@@ -1,30 +1,51 @@
 // esvcrever o código fonte do labirinto nesse arquivo aqui
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(){
-	FILE *lab; 
-	lab = fopen("teste.txt", "r");
+void salvaLabirinto (char* nomeLab, char labirinto[20][20], int linhas) {
+	FILE *lab;
+	lab = fopen(strcat(nomeLab, ".txt"), "w");
 	
-	if(lab == NULL){
-		printf("Nao foi possivel abrir o arquivo.\n");
-		return 0;
-		}
-	
-	char string[100];
-	while(fgets(string, 100, lab) != NULL){
-			printf("%s", string);
-		}	
+	for (int i = 0; i < linhas; i++){
+		fprintf(lab, "%s", strcat(labirinto[i], "\n"));
+	}
 	
 	fclose(lab);
+}
+
+int main(int argc, char** argv){ 
+	
+	int n, m;
+	char labirinto[20][20];
+	scanf("%i", &n);
+	scanf("%i", &m);
+	for (int i = 0; i < n; i++){
+		scanf("%s", labirinto[i]);
+	}
 	
 	printf("\n");
-	printf("\n");
+	for (int i = 0; i < n; i++){
+		printf("%s\n", labirinto[i]);
+	}
+
+	
+	printf("\n\n");
 	
 	printf("Selecione o modo desejado:\n");
 	printf("1. Resolver o labirinto com uma tentativa.\n");
 	printf("2. Resolver o labirinto ate obter sucesso.\n");
 	printf("3. Salvar o labirinto resolvido em um arquivo.\n");
-	printf("4. Sair do programa.");
+	printf("4. Sair do programa.\n");
+	
+	int modo;
+	scanf("%i", &modo);
+
+	switch (modo)
+	{
+		case 3: salvaLabirinto(argv[1], labirinto, n); break;
+		case 4: return 0; break;
+	}
+	
 	return 0;
 	}
