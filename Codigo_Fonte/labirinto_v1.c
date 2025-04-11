1f9f3568592542c2af066885e7ad3ed28963f879
GuilhermeAmancio <guilherme.amancio@dcomp.ufs.br>
2025-04-11 05:37:37 -0300

    Correção do loop incompleto implementado
    declaração da variável posicaoInicial num escopo mais global.
    substituição de void movimentoAleatorio por int, já que irá retornar um valor posteriormente. Modificação do case 2 ao final do código, agora já corrigido e utilizando as variáveis e funções criadas. Também é possível notar que o labirinto é impresso após o primeiro movimento, ou seja, mesmo que aparentemente os labirintos iniciais sejam diferentes, isso não é de fato verdade.

--- Codigo_Fonte/labirinto_v1.c
+++ Codigo_Fonte/labirinto_v1.c
@@ -8,6 +8,8 @@ typedef struct {
 	int x;
 	int y;
 	} Vetor2;
+	
+	Vetor2 posicaoInicial;  // Isso declara a variável (escopo mais global, será utilizada adiante)
 
 //struct para guardar as informações do personagem
 typedef struct {
@@ -27,6 +29,8 @@ typedef struct {
 	int inimigo;
 	int inicio;
 	} Elemento;
+
+
 
 //função que cria elementos com todos os valores iguais a 0 para padronização
 Elemento criaElementoZerado() {
@@ -110,9 +114,10 @@ void pausa(int ms) {
     clock_t inicio = clock(); // Marca o tempo inicial
     while ((clock() - inicio) < ms * CLOCKS_PER_SEC / 1000); // Aguarda o tempo especificado
 }
-
-void movimentoAleatorio(Personagem *heroi, Elemento *e, char labirinto[20][20], int linhas, int colunas){
+//substituição do void por int, já que a função vai retornar um valor indicando o resultado da tentativa do herói
+int movimentoAleatorio(Personagem *heroi, Elemento *e, char labirinto[20][20], int linhas, int colunas){
     srand(time(NULL) + rand());
+    
     int perdeu = 0;  // "booleano" para saber se o herói perdeu a luta
     int perdido = 0; // "booleano" para saber se o herói se perdeu
     
@@ -193,10 +198,13 @@ void movimentoAleatorio(Personagem *heroi, Elemento *e, char labirinto[20][20], 
     }
 
     if (perdido == 0 && perdeu == 0) printf("\nO heroi chegou ao destino!\n");
+    
+    if (perdeu == 1) return -1; //morreu
+    if (perdido == 1) return 0; //se perdeu
+    return 1; //chegou ao destino
 }
 
 int main(int argc, char** argv){ 
-	
 	//cria as variáveis das dimensões (n, m) e do labirinto em si (labirinto)
 	int n, m;
 	char labirinto[20][20];
@@ -231,14 +239,19 @@ int main(int argc, char** argv){
 		for (int j = 0; j < m; j++){
 			if (elementos[i][j].inicio == 1){
 				heroi.posicao = elementos[i][j].posicao; //preenche a posiçao de inicio do heroi
+				 posicaoInicial = heroi.posicao; // Salva a posição inicial para referência
 			} else if (elementos[i][j].destino == 1){
 				heroi.destino = elementos[i][j].posicao; //preenche o destino do heroi
 			}
 		}
 	}
+	
+	// Salva a posição inicial do herói para resetar depois
+Vetor2 posicaoInicial = heroi.posicao;
 	//"menu" de controle principal do código, após receber o labirinto
 	
 	int sair = 0; //variável tratada como booleana, false até que '4' seja selecionado
+	int sucesso = 0; // variável que irá controlar o loop
 	while (sair == 0){
 		//escreve as opções de uso do programa na tela
 		printf("\n\n");
@@ -262,33 +275,29 @@ int main(int argc, char** argv){
 				movimentoAleatorio(&heroi, &elementos[0][0], labirinto, n, m); 
 				break;
 			case '2':
-			system("cls");
-			int sucesso = 0; // variável que irá controlar o loop
-			while (sucesso == 0) {
-				// comando para reiniciar o labirinto, resetando os caminhos andados
-				for (int i = 0; i < n; i++) {
-					for (int j = 0; j < m; j++) {
-						if (labirinto[i][j] == '*') {
-							labirinto[i][j] = '.';
-							}
-						elementos[i][j] = classificaElemento(labirinto[i][j]);
-						elementos[i][j].posicao = (Vetor2){i, j};
-					}
+	            system("cls");
+	            while (sucesso == 0) {
+		       // Resetar o labirinto
+		        for (int i = 0; i < n; i++) {
+			     for (int j = 0; j < m; j++) {
+				  if (labirinto[i][j] == '*' || labirinto[i][j] == '!' || labirinto[i][j] == '+') {
+					labirinto[i][j] = '.';  // limpa caminhos e mortes
 				}
-				// tentativa de fazer o personagem voltar a posição inicial
-				for (int i = 0; i < n; i++) {
-					for (int j = 0; j < m; j++) {
-						if (elementos[i][j].inicio == 1) {
-							heroi.posicao = elementos[i][j].posicao;
-						}
-					}
-				}
-				movimentoAleatorio(&heroi, &elementos[0][0], labirinto, n, m);//função de movimento
-				// no momento em que ele alcança o destino, o loop é interrompido
-				if (heroi.posicao.x == heroi.destino.x && heroi.posicao.y == heroi.destino.y) {
-					sucesso = 1;
-				}
-			}	
+				elementos[i][j] = classificaElemento(labirinto[i][j]);
+				elementos[i][j].posicao = (Vetor2){i, j};
+			}
+		}
+
+		// Volta pra posição inicial salva
+		heroi.posicao = posicaoInicial;
+
+		// Executa a tentativa
+		int resultado = movimentoAleatorio(&heroi, &elementos[0][0], labirinto, n, m);
+		if (resultado == 1) {
+			sucesso = 1;
+		}
+	}
+	break;
 			case '3': salvaLabirinto(argv[1], labirinto, n, m); break;
 			case '4': sair = 1; break;
 			default: printf ("\nValor invalido!\n");
@@ -297,3 +306,4 @@ int main(int argc, char** argv){
 
 	return 0;
 }
+
